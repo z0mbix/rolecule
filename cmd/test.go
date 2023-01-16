489701c146dbe046ba4aba0fb1c0620ffda813d6
Connor Edwards <38229097+cedws@users.noreply.github.com>
2023-01-16 20:17:24 +0000

    Add no-destroy flag for test subcommand (#5)

--- cmd/test.go
+++ cmd/test.go
@@ -9,14 +9,17 @@ import (
 	"github.com/z0mbix/rolecule/pkg/config"
 )
 
+var noDestroy bool
+
 func init() {
+	testCmd.Flags().BoolVarP(&noDestroy, "no-destroy", "n", false, "don't destroy containers after completion")
 	rootCmd.AddCommand(testCmd)
 }
 
 var testCmd = &cobra.Command{
 	Use:     "test",
 	Aliases: []string{"t"},
-	Short:   "Create the container(s), converge them, test them, then clean up",
+	Short:   "Create the container(s), converge them, and test them",
 	Long: `"test" will create the containers defined, run the provisioner of choice
 against them, test them with your verifier of choice, then destroy everything.`,
 
@@ -41,9 +44,10 @@ against them, test them with your verifier of choice, then destroy everything.`,
 			log.Fatal(err.Error())
 		}
 
-		err = destroy(cfg)
-		if err != nil {
-			log.Fatal(err.Error())
+		if !noDestroy {
+			if err = destroy(cfg); err != nil {
+				log.Fatal(err.Error())
+			}
 		}
 
 		log.Info("complete")
